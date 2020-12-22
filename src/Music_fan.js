816ce304b91dbe5ecb38f0796ea9685c40c637fa
DimGiannouli <dimgiannoulh@gmail.com>
2020-12-22 16:23:23 +0000

    adding files

---
+++ src/Music_fan.js
@@ -0,0 +1,168 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "./Music_fan.css";
+import {
+  Button,
+  DataTable,
+  DataTableContent,
+  DataTableCell,
+  DataTableBody,
+  DataTableRow,
+  SimpleDialog,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogButton,
+  DialogActions,
+  IconButton
+} from "rmwc";
+import "rmwc/dist/styles";
+import spotifyIcon from "./spotifyIcon.png";
+
+class Music_fan extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      tracks: [],
+      selectedTrack: "",
+      lyricsVisible: false,
+      trackLyrics: ""
+    };
+
+    this.openSpotifyLink = this.openSpotifyLink.bind(this);
+  }
+
+  componentDidMount() {
+    const url =
+      "https://api.spotify.com/v1/artists/3WrFJ7ztbogyGnTHbHJFl2/top-tracks?market=US";
+    fetch(url, {
+      headers: {
+        Authorization:
+          "Bearer BQAb6M2ROrJHHtLuhkxRnmmhMu9utFdCHkwvGXoBXkePCq4vC8Udir_OvyYHgfofSOkXWkFazJzHtzMdpridZnHecUKiEejcN-NyVhJSb9C-1ofxLR4bSMbgGEItb5onB9faWXGZDp7n_7LLaMsT7mAd_B74KrIX92tYLAC6sTWcB28"
+      }
+    })
+      .then(res => res.json())
+      .then(result => {
+
+        this.setState({ songs: result.tracks, loading: false });
+      })
+      .catch(error => console.log(error));
+  }
+
+  openSpotifyLink(link: string) {
+    window.open(link, "_newtab");
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="title">
+          <h2>Hello Beatles fan! This is the place for you.</h2>
+          <h3>
+            {" "}
+            Below you can find Beatles top tracks and their lyrics. Enjoy!
+          </h3>
+        </div>
+
+        {this.state.loading ? (
+          <div>loading...</div>
+        ) : (
+          <div className="tracklist">
+            <DataTable style={{ backgroundColor: "rgba(255, 255, 255, 0.5)" }}>
+              <DataTableContent>
+                <DataTableBody>
+                  {this.state.songs.map(btl_tracks => (
+                    <DataTableRow>
+                      <DataTableCell
+                        style={{ color: "black", fontWeight: "bold" }}
+                      >
+                        {btl_tracks.name.substring(
+                          0,
+                          btl_tracks.name.indexOf("-")
+                        )}
+                      </DataTableCell>
+                      <DataTableCell>
+                        <Button
+                          raised
+                          style={{ backgroundColor: "#1DB954", color:"black", fontWeight: "bold" }}
+                          onClick={() => {
+                            this.openSpotifyLink(
+                              btl_tracks.external_urls.spotify
+                            );
+                          }}
+                          icon={spotifyIcon}
+                          label = "Listen on Spotify"
+                        />
+                      </DataTableCell>
+                      <DataTableCell>
+                        <Button
+                          label="Lyrics"
+                          style={{ color: "black", fontWeight: "bold" }}
+                          onClick={() => {
+                            const songName = btl_tracks.name.substring(
+                              0,
+                              btl_tracks.name.indexOf("-")
+                            );
+                            const url =
+                              "https://api.lyrics.ovh/v1/The%20Beatles/" +
+                              songName;
+                            fetch(url)
+                              .then(res => res.text())
+                              .then(result => {
+                                if (result == `{"lyrics":""}`) {
+                                  this.setState({
+                                    lyricsVisible: true,
+                                    selectedTrack: btl_tracks.name.substring(
+                                      0,
+                                      btl_tracks.name.indexOf("-")
+                                    ),
+                                    trackLyrics: "Sorry! Lyrics not found."
+                                  });
+                                } else {
+                                  this.setState({
+                                    lyricsVisible: true,
+                                    selectedTrack: btl_tracks.name.substring(
+                                      0,
+                                      btl_tracks.name.indexOf("-")
+                                    ),
+                                    trackLyrics: result
+                                  });
+                                }
+                                console.log(result);
+                              })
+                              .catch(error => console.log(error));
+                          }}
+                        />
+                      </DataTableCell>
+                    </DataTableRow>
+                  ))}
+                </DataTableBody>
+              </DataTableContent>
+            </DataTable>
+
+            <Dialog
+              open={this.state.lyricsVisible}
+              onClose={evt => {
+                this.setState({ lyricsVisible: false });
+                console.log(evt.detail.action);
+              }}
+            >
+              <DialogTitle>{this.state.selectedTrack}</DialogTitle>
+              <DialogContent>{this.state.trackLyrics}</DialogContent>
+              <DialogActions>
+                <DialogButton action="close">Cancel</DialogButton>
+                <DialogButton action="accept" isDefaultAction>
+                  Ok
+                </DialogButton>
+              </DialogActions>
+            </Dialog>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Music_fan;
---
+++ test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function tick() {
+  var element = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      'Hello, world!'
+    ),
+    React.createElement(
+      'h2',
+      null,
+      'It is ',
+      new Date().toLocaleTimeString(),
+      '.'
+    )
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
